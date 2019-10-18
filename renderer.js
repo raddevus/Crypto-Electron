b7b2e5b8440ac59e5a7821c2bbd6106c3b0d7558
raddevus <1radeutsch1@gmail.com>
2019-10-18 16:01:37 -0400

    Very basic working example with hardcoded value to leveldb file (which is in ascii format -- not utf8).  This works and displays number of bytes which is equal to number of actual bytes in file.  Good first step on path to reading file for encryption.

--- renderer.js
+++ renderer.js
@@ -1,6 +1,26 @@
+var fs = require('fs');
+alert("got it!");
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+var fileData = null;
+function readFile(){
+    
+    fs.readFile("C:\\Users\\roger.deutsch\\AppData\\Roaming\\C'YaPass\\Local Storage\\leveldb\\000006.log", 'ascii', function (err, data) {
+        if (err) return console.log(err);
+        console.log("read the file!");
+        fileData = data;
+
+        processFile();
+    });
+}
+
+function processFile(){
+    console.log("FILESIZE (bytes) : " + fileData.length);
+    console.log(fileData);
+}
+
+readFile();
