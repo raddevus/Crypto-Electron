cb2865e1f9ebfedc13c280c234b0278959baf28d
raddevus <1radeutsch1@gmail.com>
2019-10-29 08:45:23 -0400

    adding methods and changes to test using data buffer

--- js/crypt.js
+++ js/crypt.js
@@ -23,6 +23,20 @@ function encryptData(data){
     return encrypted;
 }
 
+function encryptDataBuffer(data){
+    if (data !== undefined && data != ""){
+        clearText = data;
+    }
+    key = CryptoJS.enc.Hex.parse(pwd);
+    iv = CryptoJS.enc.Hex.parse(pwd.slice(0,32));
+    encrypted = CryptoJS.AES.encrypt(clearText, key, { iv: iv }); 
+    //var encrypted = CryptoJS.AES.encrypt(clearText, "Secret"); 
+    //var cipherText = CryptoJS.enc.Hex.parse(String(encrypted.ciphertext));
+    //console.log("cipherText : " + cipherText);
+    document.getElementById("encrypted").innerHTML = encrypted;
+    return encrypted;
+}
+
 function decryptData(data){
     if (data !== undefined && data != ""){
         encrypted = data;
@@ -53,25 +67,42 @@ function decryptFile(){
         createOutputFileFromInputData();
     }
 }
-
+let xdata = undefined;
 function createOutputFileFromInputData(){
     var currentSelectedFile = $('#selected-file').text();
     if (currentSelectedFile == null){
         return;
     }
     alert(currentSelectedFile);
-    fs.readFile(currentSelectedFile, 'ascii', function (err, data) {
-        if (err) return console.log(err);
-        console.log("read the file!");
-        if (isEncrypting){
-            outputFileData = encryptData(data);
-        }
-        else{
-            outputFileData = decryptData(data);
-        }
-        //processFile();
-        writeTargetFile(outputFileData);
-    });
+    if ($("#useDataBufferCheckBox").attr('checked') || $("#useDataBufferCheckBox").prop('checked')){
+        fs.readFile(currentSelectedFile, function (err, data) {
+            if (err) return console.log(err);
+            console.log("read the file!");
+            xdata = data;
+            if (isEncrypting){
+                outputFileData = encryptDataBuffer(data.asciiSlice(0,data.length));
+            }
+            else{
+                outputFileData = decryptData(data);
+            }
+            //processFile();
+            writeTargetFile(outputFileData);
+        });
+     }
+    else{
+        fs.readFile(currentSelectedFile, 'ascii', function (err, data) {
+            if (err) return console.log(err);
+            console.log("read the file!");
+            if (isEncrypting){
+                outputFileData = encryptData(data);
+            }
+            else{
+                outputFileData = decryptData(data);
+            }
+            //processFile();
+            writeTargetFile(outputFileData);
+        });
+    }
 }
 
 function decodeHexString(stringOfHexBytes){
